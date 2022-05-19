34ca8a0aa27289f1dd1558707d35fb7743e468e5
Jason Banks <48374544+identity-jason@users.noreply.github.com>
2022-05-19 04:07:05 +0100

    Add files via upload

---
+++ planets/planets.py
@@ -0,0 +1,150 @@
+from imp import PY_CODERESOURCE
+from operator import truediv
+import pygame
+import math
+
+# -- https://www.youtube.com/watch?v=WTLPmUHTPqo
+# -- https://fiftyexamples.readthedocs.io/en/latest/gravity.html
+
+pygame.init()
+
+WIDTH, HEIGHT = 800,800
+
+win = pygame.display.set_mode((WIDTH,HEIGHT))
+
+WHITE = (255,255,255)
+YELLOW = (255,255,0)
+BLUE = (100,149,237)
+BLACK = (0,0,0)
+RED = (188,39,50)
+DARK_GRAY = (80,78,81)
+GREEN = (0,255,0)
+
+FONT = pygame.font.SysFont("comicsans", 16)
+
+pygame.display.set_caption("planets")
+
+class Planet:
+    AU = 149.6e6 * 1000
+    G = 6.67428e-11
+    SCALE = 225/AU
+    TIMESTEP = 3600 * 24
+    
+    def __init__(self, x,y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color 
+        self.mass = mass
+
+        self.x_vel = 0
+        self.y_vel = 0
+
+        self.sun = False
+        self.distance_to_sun = 0
+
+        self.orbit = []
+        self.moons = [self]        
+
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH/2
+        y = self.y * self.SCALE + HEIGHT/2
+
+        if len(self.orbit)>2:
+            updated_points = []
+            for point in self.orbit:
+                x,y = point
+                x = x * self.SCALE + WIDTH/2
+                y = y * self.SCALE + HEIGHT/2
+                updated_points.append((x,y))
+
+            pygame.draw.lines(win, self.color, False, updated_points)
+
+
+        pygame.draw.circle(win, self.color, (x,y), self.radius)
+        #if not self.sun:
+        #    distance_text = FONT.render(f"{round(self.distance_to_sun/1000, 1)}km",1,WHITE)
+        #    win.blit(distance_text, (x - distance_text.get_width()/2,y - distance_text.get_height()/2))        
+
+    def attraction(self, other):
+        other_x, other_y = other.x, other.y
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = math.sqrt(distance_x **2 + distance_y **2)
+
+        if other.sun:
+            self.distance_to_sun = distance
+
+        force = self.G * self.mass * other.mass / distance**2
+
+        theta = math.atan2(distance_y, distance_x)
+
+        force_x = math.cos(theta) * force
+        force_y = math.sin(theta) * force
+
+        return force_x, force_y
+
+    def update_position(self, planets):
+        total_fx = total_fy = 0
+
+        for planet in planets:
+            if self == planet:
+                continue
+            fx,fy = self.attraction(planet)
+
+            total_fx += fx
+            total_fy += fy
+
+        self.x_vel += total_fx / self.mass * self.TIMESTEP
+        self.y_vel += total_fy / self.mass * self.TIMESTEP
+
+        self.x += self.x_vel * self.TIMESTEP
+        self.y += self.y_vel * self.TIMESTEP
+
+        self.orbit.append((self.x, self.y))
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    sun = Planet(0,0,30, YELLOW, 1.98892 * 10**30)
+    sun.sun = True
+
+    earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+    earth.y_vel = 29.783*1000
+
+    moon = Planet(-1.0 * Planet.AU, Planet.AU / 384400, 4, WHITE, 7.34*10**22)
+    moon.y_vel = 29.783*10
+
+    mars = Planet(-1.524*Planet.AU, 0, 12, RED, 6.39 * 10**23)
+    mars.y_vel = 24.077*1000
+    mercury = Planet(-0.387*Planet.AU, 0,8, DARK_GRAY, 3.30*10**23)
+    mercury.y_vel = 47.4*1000
+    venus = Planet(-0.723 * Planet.AU, 0,14, GREEN, 4.8685 * 10**24)
+    venus.y_vel = 35.02*1000
+
+
+    planets = [sun,earth, mars,mercury,venus]
+    #planets = [sun,earth, mars,mercury,venus,moon]
+#    planets = [sun,earth,moon]
+
+    while run:
+        clock.tick(60)
+
+        win.fill(BLACK)
+        #pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        for planet in planets:
+            planet.update_position(planets)
+            planet.draw(win)
+
+        pygame.display.update()        
+
+    pygame.quit()
+
+main()
